d9a0c159c2d113f7ee2d61b51893049146b620a8
its-alikhoker <iamaliraza777@gmail.com>
2025-01-25 22:11:05 +0500

    fixes

--- ksa/saudi_arabia/wizard/operations/on_cancel_si_pi.py
+++ ksa/saudi_arabia/wizard/operations/on_cancel_si_pi.py
@@ -1,9 +1,10 @@
 import frappe
 def prevent_cancel_if_qr(doc, method):
-    """
-    Prevent cancelation if `ksa_einv_qr` field exists in the document.
-    """
-    if doc.get('ksa_einv_qr'):
-        frappe.throw(
-            ("This document cannot be canceled or edited as per ZATCA compliance. This invoice must be submitted to the ZATCA authority")
-        )
+    pass
+    # """
+    # Prevent cancelation if `ksa_einv_qr` field exists in the document.
+    # """
+    # if doc.get('ksa_einv_qr'):
+    #     frappe.throw(
+    #         ("This document cannot be canceled or edited as per ZATCA compliance. This invoice must be submitted to the ZATCA authority")
+    #     )
